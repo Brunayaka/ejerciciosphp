02b2921408723618c9f425effc77308a48c9ecba
Bruno Mozota <Brunocadrete@gmail.com>
2023-10-04 18:55:11 +0200

    Explicaciones y ejercicios

---
+++ ejclase/autoriza.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST["ienviar"]) && !empty($_POST["ienviar"])){
+    if (isset($_POST["iusuario"]) && !empty($_POST["iusuario"])) {
+        $usuario = $_POST["iusuario"];
+    }
+    if (isset($_POST["ipassword"]) && !empty($_POST["ipassword"])) {
+        $password = $_POST["ipassword"];
+    }
+
+    if (!empty($usuario) && !empty($password)) {
+        if($usuario === "Admin" && $password === "456"){
+            echo "<br>Bienvenido administrador";
+        }else{
+            echo "<br>Bienvenido usuario";
+        }
+    }else{
+        echo "<h4>No has especificado usuario y/o password.</h4>";
+    }
+}else{
+    echo "<h2>No has llegado aqui haciendo login</h2>";
+}
---
+++ ejclase/encuesta.php
@@ -0,0 +1,26 @@
+<?php
+echo "<h2>Resultado encuesta Lenguajes Programacion</h2>";
+if (isset($_POST["ienviar"]) && !empty($_POST["ienviar"])) {
+    //chequeo del radio button
+    if (isset($_POST["lprog"]) && !empty($_POST["lprog"])) {
+        $lenguaje = $_POST["lprog"];
+        echo "<h2>Tu lenguaje preferido es: $lenguaje</h2>";
+    }
+
+
+    //chequeo del checkBox
+    //echo "<br>Num eleme : " . count($_POST["ntrans"]);
+    if (isset($_POST["ntrans"]) && !empty($_POST["ntrans"])) {
+        $transportes = $_POST["ntrans"];
+        foreach ($transportes as $key) {
+            echo "<br>Tu transporte preferido es: " . $key;
+        }
+    } else {
+        echo "<br>No has seleccionado ningun transporte";
+    }
+    //chequeo seleccion 
+    if (isset($_POST["sfutbol"]) && !empty($_POST["sfutbol"])) {
+        $efutbol = $_POST["sfutbol"];
+        echo "<h2>Tu equipo preferido es: $efutbol</h2>";
+    }
+}
